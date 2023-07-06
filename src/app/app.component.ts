f69c362c70d7615f87e7036042ea6f548a9c5bf1
muhammed-sayeed <sayeedmon25@gmail>
2023-07-06 17:24:16 +0530

    -api connected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { Observable} from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+
+
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  constructor(private service:WeatherService){
+
+  }
+  cityName:string = 'Kozhikode'
+  weatherData:any
+  city?:string
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName)
+    this.city = this.cityName
+  }
+
+  onSubmit(){
+    this.getWeatherData(this.cityName)
+    this.city = this.cityName
+    this.cityName = ''
+   
+  }
+
+  private getWeatherData(cityName:string){
+    this.service.weatherData(cityName).subscribe((data:any)=>{
+      this.weatherData = data.main
+      console.log(this.weatherData);
+      
+       
+    })
+  }
+  
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/environment';
+import { Observable} from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,11 @@ export class WeatherService {
   constructor(private http:HttpClient) { 
     
   }
+
+  weatherData(city: string) {
+    const apiKey = environment.apiKey;
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+  }
+  
+ 
 }
